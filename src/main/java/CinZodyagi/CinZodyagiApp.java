9d057a2a20361ca601ef016b2c3f9a2d0f9e9ab8
sarperkaya01 <sarperkaya01@gmail.com>
2025-07-19 01:00:55 +0300

    cinzodi.java file add

--- src/main/java/CinZodyagi/CinZodyagiApp.java
+++ src/main/java/CinZodyagi/CinZodyagiApp.java
@@ -1,4 +1,22 @@
 package CinZodyagi;
 
+import java.util.Scanner;
+
 public class CinZodyagiApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("Lutfen bir yil giriniz : ");
+            int date = sc.nextInt();
+            String[] zodiac = {"Maymun", "Horoz", "Köpek", "Domuz", "Fare", "Öküz", "Kaplan", "Tavşan", "Ejderha", "Yılan", "At", "Koyun"};
+            for (int i = 0; i < zodiac.length; i++) {
+                if (date % 12 == i) {
+                    System.out.println("Cin burcunuz : " + zodiac[i]);
+
+                }
+            }
+        }
+    }
 }
