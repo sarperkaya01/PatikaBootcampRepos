75f0de92de7ec120c56e11100ef4be2222cc45f4
sarperkaya01 <sarperkaya01@gmail.com>
2025-07-20 16:35:04 +0300

    tersucgen

--- src/main/java/TersUcgen/TersUcgenApp.java
+++ src/main/java/TersUcgen/TersUcgenApp.java
@@ -1,4 +1,22 @@
 package TersUcgen;
 
+import java.util.Scanner;
+
 public class TersUcgenApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Olusturmak istediginiz ucgen icin basamak sayisi girin : ");
+
+        int triSize = sc.nextInt();
+        String star = "";
+
+        for (int i = triSize; i > 0; i--) {
+            for (int j =  0; j < 2*i-1; j++) {
+                star += "*";
+            }
+            System.out.println(star);
+            star = "";
+        }
+
+    }
 }
